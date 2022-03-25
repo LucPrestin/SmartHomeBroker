173adcaf128588754d7cfd3a12a1a2cfcc066c3f
LucPrestin <l.prestin@gmx.de>
2022-03-25 10:52:43 +0100

    fix endless loop

--- add_user.sh
+++ add_user.sh
@@ -1,7 +1,15 @@
+read -r -p "Enter user name: " user
+sudo mosquitto_passwd -c /etc/mosquitto/passwd $user
+
 while : ; do
-    read -r -p "Do you want to add a client user? [y/n] " response
+    read -r -p "Do you want to add another client user? [y/n] " response
     if [[ $response =~ ^[yY]$ ]]; then
         read -r -p "Enter user name: " user
         sudo mosquitto_passwd -c /etc/mosquitto/passwd $user
+    else
+        break
     fi
 done
+
+sudo systemctl restart mosquitto.service
+
